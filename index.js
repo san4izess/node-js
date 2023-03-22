e7525291eb168fc6c55e1029a19f90898c243282
san4izess <san4izess@gmail.com>
2023-03-22 22:26:57 +0200

    add a fo cycle and optimize code

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ console.log("hello AI bot");
 console.log("hello");
 console.log("dqwdsadasd");
 
-for (i = 1000; i < 0; i - 7) {
-  console.log(i);
-}
 console.log("hello");
 console.log("dqwdsadasd");
+for (let i = 1000; i > 0; i -= 7) {
+  console.log(i);
+}
