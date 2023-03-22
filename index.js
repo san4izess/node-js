538f836107932cb252e06958e8c4d8b8650ea8a3
san4izess <san4izess@gmail.com>
2023-03-22 22:37:15 +0200

    added a cycle for 1000-7

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-console.log("hello AI bot");
-//caches.push;
-console.log("hello");
-console.log("dqwdsadasd");
-
-console.log("hello");
-console.log("dqwdsadasd");
-for (let i = 1000; i > 0; i -= 7) {
+for (let i = 1000; i >= 0; i -= 7) {
   console.log(i);
 }
