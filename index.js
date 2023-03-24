c013f2ce6bedfa31750b7c086a6be859a84ab471
Sasha Chernenko <san4izess@gmail.com>
2023-03-24 11:02:42 +0200

    added eps

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ function F2(x) {
   return Math.log(x);
 }
 
-function integrateSimpson(f, a, b, n) {
-  const h = (b - a) / n;
+function integrateSimpson(f, a, b, n, eps) {
+  let h = (b - a) / n;
   let sum = f(a) + f(b);
+  let x = 0;
   for (let i = 1; i < n; i++) {
-    const x = a + i * h;
+    x = a + i * h;
     sum += f(x) * (i % 2 === 0 ? 2 : 4);
   }
-  return (sum * h) / 3;
+  return (h / 3) * (sum + 4 * f(x - h) - 2 * f(x - 2 * h));
 }
 
 const A = 0;
@@ -21,7 +22,7 @@ const B = Math.PI / 6;
 const C = 1;
 const D = 3.5;
 const N = 40;
-
+const eps = 0.0002;
 const I1 = integrateSimpson(F1, A, B, N);
 const I2 = integrateSimpson(F2, C, D, N);
 
