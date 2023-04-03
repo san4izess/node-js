ff21b211d2e65784b0f268294db912f713a8bcb1
San4izess <san4izess@gmail.com>
2023-04-03 19:52:44 +0300

    lesson04 done

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const fs = require("fs");
 
 fs.readFile("file.txt", "utf-8", (error, data) => {
-  fs.writeFile("test2.txt", `${data}add a example text`, () => {});
+  fs.mkdirSync("./files", () => {});
+  fs.writeFileSync(
+    "./files/test2.txt",
+    `${data}add a example text`,
+    (error) => {
+      error ? console.log(error) : null;
+    }
+  );
 });
+
+setTimeout(() => {
+  fs.unlink("files/test2.txt", () => {});
+}, 4000);
+
+setTimeout(() => {
+  fs.rmdir("files", () => {});
+}, 6000);
